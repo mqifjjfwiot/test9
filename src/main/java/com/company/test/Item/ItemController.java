a5ca8e657d30803999af94216c8fbf12a6fb5bb6
서장원 <sjwonsj95@gmail.com>
2021-06-28 11:00:01 +0900

    210628 서장원 수정본

---
+++ src/main/java/com/company/test/Item/ItemController.java
@@ -0,0 +1,39 @@
+package com.company.test.Item;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+
+@Controller
+public class ItemController {
+	
+	//메인으로
+	@RequestMapping("/Item/Items.do")
+	public String Items(@RequestParam int menu) {
+		switch(menu) {
+		case 1: return "home";
+		case 2: return "home";
+		case 3: return "home";
+		case 4: return "home";
+		case 5: return "home";
+		case 6: return "home";
+		case 7: return "home";
+		case 8: return "home";
+		case 9: return "home";
+		case 10: return "home";
+		case 11: return "home";
+		case 12: return "home";
+		case 13: return "home";
+		case 14: return "home";
+		case 15: return "home";
+		case 16: return "home";
+		case 17: return "home";
+		case 18: return "home";
+		default : return "home";
+		}
+	}
+
+}
