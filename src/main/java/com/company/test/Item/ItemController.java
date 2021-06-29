97a76dc7ce223d16afcde3305f5e9fb9c1c8a099
서장원 <sjwonsj95@gmail.com>
2021-06-30 00:59:40 +0900

    210630카테고리별 상품페이지

--- src/main/java/com/company/test/Item/ItemController.java
+++ src/main/java/com/company/test/Item/ItemController.java
@@ -1,5 +1,7 @@
 package com.company.test.Item;
 
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -12,28 +14,29 @@ public class ItemController {
 	
 	//메인으로
 	@RequestMapping("/Item/Items.do")
-	public String Items(@RequestParam int menu) {
+	public String Items(@RequestParam int menu,Map map) {
 		switch(menu) {
-		case 1: return "home";
-		case 2: return "home";
-		case 3: return "home";
-		case 4: return "home";
-		case 5: return "home";
-		case 6: return "home";
-		case 7: return "home";
-		case 8: return "home";
-		case 9: return "home";
-		case 10: return "home";
-		case 11: return "home";
-		case 12: return "home";
-		case 13: return "home";
-		case 14: return "home";
-		case 15: return "home";
-		case 16: return "home";
-		case 17: return "home";
-		case 18: return "home";
-		default : return "home";
+		case 1: map.put("message", "신상품");break;
+		case 2: map.put("message", "행사상품");break;
+		case 3: map.put("message", "육류/계란");break;
+		case 4: map.put("message", "생선/어패류");break;
+		case 5: map.put("message", "과일");break;
+		case 6: map.put("message", "채소");break;
+		case 7: map.put("message", "쌀/잡곡/견과");break;
+		case 8: map.put("message", "밀키트");break;
+		case 9: map.put("message", "냉동/냉장/통조림");break;
+		case 10: map.put("message", "레토르트");break;
+		case 11: map.put("message", "육/어육가공품");break;
+		case 12: map.put("message", "김치");break;
+		case 13: map.put("message", "라면/면류");break;
+		case 14: map.put("message", "베이커리");break;
+		case 15: map.put("message", "과자/시리얼");break;
+		case 16: map.put("message", "생수");break;
+		case 17: map.put("message", "주스/탄산");break;
+		case 18: map.put("message", "차");break;
+		default : map.put("message", "주류");break; 
 		}
+		return "ProductPage";
 	}
 
 }
