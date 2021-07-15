adc43b82f7ca104661d365fd74eabe78a94507dd
서장원 <sjwonsj95@gmail.com>
2021-07-15 19:02:50 +0900

    ItemsDTO추가

---
+++ src/main/java/com/company/test/service/ItemsDTO.java
@@ -0,0 +1,35 @@
+package com.company.test.service;
+
+import java.io.File;
+import java.util.List;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ItemsDTO {
+	private String INO;
+	private String INAME;
+	private String INAME2;
+	private String INAME3;
+	private String PRICE;
+	private String CATEGORY;
+	private String STOCK;
+	private String DISH;
+	private String HASHTAG;
+	private String BRAND;
+	private File IMAGE;
+	private File IMAGE2;
+	private File IPAGE;
+	private File IPAGE2;
+	
+}
