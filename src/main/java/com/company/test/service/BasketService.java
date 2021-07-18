7c26176c30957cdac25e7d3dbb96a4985b096e63
KSH <mqifjjfwiot@naver.com>
2021-07-18 16:26:50 +0900

    바구니 DAO impl 생성

--- src/main/java/com/company/test/service/BasketService.java
+++ src/main/java/com/company/test/service/BasketService.java
@@ -7,6 +7,7 @@ import javax.servlet.http.HttpServletRequest;
 
 public interface BasketService {
 	//상세보기용]
+	//장바구니 생성
 	BasketDTO selectOne(Map map);
 	//입력/수정/삭제용]
 	int insert(Map map);
---
+++ src/main/java/com/company/test/service/impl/BasketDAO.java
@@ -0,0 +1,52 @@
+package com.company.test.service.impl;
+
+
+import java.io.IOException;
+import java.io.Reader;
+import java.util.List;
+import java.util.Map;
+
+import javax.annotation.Resource;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.stereotype.Repository;
+
+import com.company.test.service.ItemsDTO;
+
+
+@Repository
+public class BasketDAO  {
+	
+	@Resource(name="itemstemplate")
+	private SqlSessionTemplate sqlMapper;
+	
+	
+	public ItemsDTO selectOne(Map map) {		
+		return sqlMapper.selectOne("itemsSelectOne", map);
+	}
+	
+	public ItemsDTO shopping(Map map) {		
+		return sqlMapper.selectOne("itemsShopping",map);
+	}
+	
+	public int insert(Map map) {
+		
+		return sqlMapper.insert("itemsInsert", map);
+	}
+
+	
+	public int delete(Map map) {
+		
+		return sqlMapper.delete("itemsDelete",map);
+	}
+
+	
+	public int update(Map map) {		
+		return sqlMapper.update("itemsUpdate",map);
+	}
+
+}
